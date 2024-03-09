32dc24cc60a30739bc9b45a04e6552610db90a99
Will Kusper <wkusper@iu.edu>
2024-03-08 23:30:51 -0500

    commit

--- src/main/java/edu/iu/habahram/pizzashop/model/NewYorkPizzaFactory.java
+++ src/main/java/edu/iu/habahram/pizzashop/model/NewYorkPizzaFactory.java
@@ -1,4 +1,23 @@
 package edu.iu.habahram.pizzashop.model;
 
-public class NewYorkPizzaFactory {
+public class NewYorkPizzaFactory implements PizzaFactory{
+    @Override
+    public Pizza createPizza(String type) {
+        Pizza pizza = null;
+        PizzaIngredientFactory ingredientFactory = new NewYorkPizzaIngredientFactory();
+        if (type.equals("cheese")) {
+            pizza = new CheesePizza(ingredientFactory);
+            pizza.setName("New York Style Cheese Pizza");
+        } else if (type.equals("veggie")) {
+            pizza = new VeggiePizza(ingredientFactory);
+            pizza.setName("New York Style Veggie Pizza");
+        } else if (type.equals("clam")) {
+            pizza = new ClamPizza(ingredientFactory);
+            pizza.setName("New York Style Clam Pizza");
+        } else if (type.equals("pepperoni")) {
+            pizza = new PepperoniPizza(ingredientFactory);
+            pizza.setName("New York Style Pepperoni Pizza");
+        }
+        return pizza;
+    }
 }
--- src/main/java/edu/iu/habahram/pizzashop/model/NewYorkPizzaStore.java
+++ src/main/java/edu/iu/habahram/pizzashop/model/NewYorkPizzaStore.java
@@ -2,7 +2,7 @@ package edu.iu.habahram.pizzashop.model;
 
 public class NewYorkPizzaStore extends PizzaStore{
     protected Pizza createPizza(String item) {
-        PizzaFactory pizzaFactory = new ChicagoPizzaFactory();
+        PizzaFactory pizzaFactory = new NewYorkPizzaFactory();
         return pizzaFactory.createPizza(item);
     }
 }
